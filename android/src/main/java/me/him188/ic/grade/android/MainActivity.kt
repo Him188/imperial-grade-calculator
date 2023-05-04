028932c9cfe871aaba61f39d79d47aa444647c54
Him188 <Him188@mamoe.net>
2023-05-05 00:30:50 +0100

    android

--- android/src/main/java/me/him188/ic/grade/android/MainActivity.kt
+++ android/src/main/java/me/him188/ic/grade/android/MainActivity.kt
@@ -3,15 +3,18 @@ package me.him188.ic.grade.android
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
+import me.him188.ic.grade.common.Computing
 import me.him188.ic.grade.common.MainWindow
+import me.him188.ic.grade.common.result.AcademicYearResult
+import me.him188.ic.grade.common.result.StandaloneModuleResult
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                MainWindow()
+                MainWindow(AcademicYearResult(Computing.Year2.modules.map { StandaloneModuleResult(it) }))
             }
         }
     }
