3b1bcb4fa5c03287b22633471d833942eccf0666
Him188 <Him188@mamoe.net>
2023-05-05 19:29:23 +0100

    Add signing

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,7 @@
+@file:Suppress("UnstableApiUsage")
+
+import java.util.*
+
 plugins {
     id("org.jetbrains.compose")
     id("com.android.application")
@@ -25,9 +29,63 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    signingConfigs {
+        kotlin.runCatching { getProperty("signing_release_storeFileFromRoot") }.getOrNull()?.let {
+            create("release") {
+                storeFile = rootProject.file(it)
+                storePassword = getProperty("signing_release_storePassword")
+                keyAlias = getProperty("signing_release_keyAlias")
+                keyPassword = getProperty("signing_release_keyPassword")
+            }
+        }
+        kotlin.runCatching { getProperty("signing_release_storeFile") }.getOrNull()?.let {
+            create("release") {
+                storeFile = file(it)
+                storePassword = getProperty("signing_release_storePassword")
+                keyAlias = getProperty("signing_release_keyAlias")
+                keyPassword = getProperty("signing_release_keyPassword")
+            }
+        }
+    }
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.findByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                projectDir.resolve("proguard-rules.pro").also {
+                    check(it.exists()) { "Could not find ${it.absolutePath}" }
+                }
+            )
         }
+        getByName("debug") {
+            isMinifyEnabled = false
+            applicationIdSuffix = ".debug"
+        }
+    }
+}
+
+
+
+fun getProperty(name: String) =
+    System.getProperty(name)
+        ?: System.getenv(name)
+        ?: properties[name]?.toString()
+        ?: getLocalProperty(name)
+        ?: ext.get(name).toString()
+
+
+val Project.localPropertiesFile get() = project.rootProject.file("local.properties")
+
+fun Project.getLocalProperty(key: String): String? {
+    return if (localPropertiesFile.exists()) {
+        val properties = Properties()
+        localPropertiesFile.inputStream().buffered().use { input ->
+            properties.load(input)
+        }
+        properties.getProperty(key)
+    } else {
+        localPropertiesFile.createNewFile()
+        null
     }
 }
